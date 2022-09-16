0ee447f4ebe3cb73de7881dc502b9459315706a7
Taylor Jadin <jadin.taylor@gmail.com>
2022-09-16 15:02:25 -0500

    more info

--- hello.py
+++ hello.py
@@ -4,5 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-    return "<p>For more info check out: https://github.com/TaylorJadin/hello-world-flask</p>"
+    return "<p>Hello, World!</p><p>For more info check out: https://github.com/TaylorJadin/hello-world-flask</p>"
