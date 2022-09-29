1e079f78c0e2e6715cbafff42e0f78c828180208
Taylor Jadin <jadin.taylor@gmail.com>
2022-09-29 11:40:54 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p><p><a href='https://github.com/TaylorJadin/hello-world-flask'>More info</a></p>"
+    return "<p>Hello, World!</p><p><a href='https://support.reclaimhosting.com/hc/en-us/articles/9220813439127'>More info</a></p>"
